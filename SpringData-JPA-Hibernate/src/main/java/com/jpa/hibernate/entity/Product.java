ab42dffb19f910e4d0e3ca4c42f8bcc5ce5de532
Ram Tiwari <ramtiwari8716@gmail.com>
2024-04-20 18:25:19 +0530

    Commit 2 : Modified entity for Product table

--- SpringData-JPA-Hibernate/src/main/java/com/jpa/hibernate/entity/Product.java
+++ SpringData-JPA-Hibernate/src/main/java/com/jpa/hibernate/entity/Product.java
@@ -1,18 +1,25 @@
 package com.jpa.hibernate.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 @Entity
-@Table(name = "product_db")
+@Table(name = "products_db")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
-    @Id
-    private Long id;
-    private String sku; //stock keeping unit
-    private String name;
+     @Id
+     @GeneratedValue(strategy = GenerationType.AUTO)
+     private Long id;
+     @Column(name = "stock_keeping_unit")
+     private String sku; //stock keeping unit
+     @Column(nullable = false)
+     private String name;
      private  String description;
      private boolean active;
      private String imageUrl;
