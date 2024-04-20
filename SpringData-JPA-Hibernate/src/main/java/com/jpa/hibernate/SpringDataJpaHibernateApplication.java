2b1a34076802b8d494ec2a48c6cc3aef9f7891c4
Ram Tiwari <ramtiwari8716@gmail.com>
2024-04-20 17:45:21 +0530

    Springboot data jpa project has been created

---
+++ SpringData-JPA-Hibernate/src/main/java/com/jpa/hibernate/SpringDataJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaHibernateApplication.class, args);
+	}
+
+}
